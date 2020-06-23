bcb902ebbba4d9270af3ca723986667e2dc692a5
Ivan <naviivan321@gmail.com>
2020-06-22 23:04:08 -0600

    started merging communities

---
+++ merging_communitites.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self,community):
+        self.community = community
+class Community:
+    def __init__(self,members):
+        self.size = len(memebers)
+        self.memebers = members
+    
+
