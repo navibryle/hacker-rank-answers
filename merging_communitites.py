d61039c88cc5288cdf9b844c4c648bd041691be4
Ivan <naviivan321@gmail.com>
2020-06-25 14:11:50 -0600

    finished merging communities and got all the points :

--- merging_communitites.py
+++ merging_communitites.py
@@ -1,42 +1,47 @@
-
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-#passes the sample test case but fails all the other ones
+#issue output is incorrect
+#possible cause the old member of the list is outdated so we should have it only point to one root
 import fileinput
 class Community:
     def __init__(self,representative):
         self.representative = representative
         self.size = 1
-    def addMember(self):
+    def addMember(self,index):
         self.size += 1
+        self.members.append(index)
     def get_size(self):
         return self.size
+def findRoot(member,parent):
+    #member will be an index
+    root = member
+    while parent[root-1].representative != root:
+        root = parent[root-1].representative
+    return root
 def mergingQuery(member1,member2,parent):
-    if isinstance(member1,Community) and isinstance(member2,Community):
-        newRepresentative = member1.representative
-        member1.size += member2.size
-        for index in member2.members:
-            parent[index] = member1
-    elif isinstance(member1,Community) and isinstance(member2,int):
-        
-        member1.size += 1
-        parent[member2-1] = member1
-    elif isinstance(member2,Community) and isinstance(member1,int):
-        
-        member2.size += 1
-        parent[member1-1] = member2
-    else:
-        #they are both integers
-        newCommunity = Community(member1)
-        newCommunity.size += 1
-        parent[member1-1] = newCommunity
-        parent[member2-1] = newCommunity
+    '''
+    find the root of the community with smaller members and have it point to the root of the community with more members
+    the root will be the single source of truth for the sizes of the communities.
+    '''
+    member1Root = parent[findRoot(member1.representative,parent)-1]
+    member2Root = parent[findRoot(member2.representative,parent)-1]
+    if member1Root.representative != member2Root.representative:
+        if (member1Root.size > member2Root.size):
+            member1Root.size += member2Root.size
+            member2Root.representative = member1Root.representative
+        else:
+            member2Root.size += member1Root.size
+            member1Root.representative = member2Root.representative
+def make_to_community(anInt):
+    return Community(anInt)
 def main():
     query = []
     parent = []
     for line in fileinput.input():
         
         if line[0].isdigit():
-            parent2 = [item+1 for item in range(int(line[0]))]
+            lineSplit = line.split()
+            #item+1 so that the representative when we turn these integers into communities are correct
+            parent2 = list(map(make_to_community,[item+1 for item in range(int(lineSplit[0]))]))
             parent = parent2
         else:
             query.append(line.rstrip())
@@ -53,11 +58,9 @@ def main():
             member2 = parent[int(querySplit[2])-1]
             mergingQuery(member1,member2,parent)#this will mutate parent
         else:
-
+            
             querySplit = item.split()
             member = parent[int(querySplit[1])-1]
-            if isinstance(member,int):
-                print(1)
-            else:
-                print(member.size)
+            memberRoot = parent[findRoot(member.representative,parent)-1]
+            print(memberRoot.size)
 main()
